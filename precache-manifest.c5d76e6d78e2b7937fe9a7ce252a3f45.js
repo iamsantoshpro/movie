84917e504a79c12c53cab4f5555bacf58089de6a
iamsantoshpro <santosh.bm302@gmail.com>
2020-01-19 18:39:58 +0530

    Updates

---
+++ precache-manifest.c5d76e6d78e2b7937fe9a7ce252a3f45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b2d4cedf3bf434e544924ded6a146f",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "a7c4d766a2b1056373a3",
+    "url": "/movie/static/css/main.739aba6a.chunk.css"
+  },
+  {
+    "revision": "b9fc454640008ec06a45",
+    "url": "/movie/static/js/2.64982870.chunk.js"
+  },
+  {
+    "revision": "a7c4d766a2b1056373a3",
+    "url": "/movie/static/js/main.5e540ee3.chunk.js"
+  },
+  {
+    "revision": "8b5295b2510a28f6a2e8",
+    "url": "/movie/static/js/runtime-main.4ddc0f32.js"
+  }
+]);
